d8ade1d336d18331a9f8a9d17f595fa0e9dd6436
Eduarda Scharnhorst <eduardascharnhorst@gmail.com>
2020-10-12 19:55:58 -0300

    video-3.2

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 
 import App, { calcularNovoSaldo } from './app';
 
@@ -32,6 +32,26 @@ describe('Componente principal', () => {
 
       expect(novoSaldo).toBe(100);
     });
+    it('que é um saque, a transação deve ser realizada', () => {
+      const {
+        getByText,
+        getByTestId,
+        getByLabelText
+      } = render(<App />);
+
+      const saldo = getByText('R$ 1000');
+      const transacao =  getByLabelText('Saque');
+      const valor = getByTestId('valor');
+      const botaoTransacao = getByText('Realizar operação');
+
+      expect(saldo.textContent).toBe('R$ 1000')
+
+      fireEvent.click(transacao, { target: { value: 'saque'}});
+      fireEvent.change(valor, { target: { value:  10 }});
+      fireEvent.click(botaoTransacao);
+
+      expect(saldo.textContent).toBe('R$ 990');
+    })
   })
 })
 
