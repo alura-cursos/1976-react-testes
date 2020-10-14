9b3544ba3d6a3191d86465352244c5ac034b80d4
Eduarda Scharnhorst <eduardascharnhorst@gmail.com>
2020-10-14 13:17:57 -0300

    video-4.3

--- src/AppIntegracoes.test.js
+++ src/AppIntegracoes.test.js
@@ -6,17 +6,17 @@ import App from './App';
 jest.mock('./api')
 
 describe('Requisições para API', () => {
-  it('Exibir lista de transações através da API', () => {
+  it('Exibir lista de transações através da API', async () => {
     api.listaTransacoes.mockResolvedValue([
       {
-        "valor": 10,
+        "valor": "10",
         "transacao": "saque",
         "data": "10/08/2020",
         "id": 1
       },
       {
         "transacao": "deposito",
-        "valor": 20,
+        "valor": "20",
         "data": "26/09/2020",
         "id": 2
       }
@@ -24,7 +24,9 @@ describe('Requisições para API', () => {
 
     render(<App />);
 
-    expect(screen.getByTestId('transacoes').children.lenght).toBe(2)
+    expect(await screen.findByText('saque')).toBeInTheDocument();
+
+    expect(screen.getByTestId('transacoes').children.length).toBe(2)
 
   });
 })
