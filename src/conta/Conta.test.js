17b6c4d2e9b732dbfeaabf7f88088b7dee40cd0e
Eduarda Scharnhorst <eduardascharnhorst@gmail.com>
2020-10-14 13:46:53 -0300

    video-5.1

--- src/conta/Conta.test.js
+++ src/conta/Conta.test.js
@@ -1,5 +1,5 @@
 import React from  'react';
-import { render, screen }  from '@testing-library/react';
+import { render, screen, fireEvent }  from '@testing-library/react';
 import Conta from './Conta';
 
 describe('Componente de conta', () => {
@@ -9,5 +9,15 @@ describe('Componente de conta', () => {
     const saldo = screen.getByTestId('saldo-conta');
 
     expect(saldo.textContent).toBe('R$ 1000');
+  })
+
+  it('Chama a função de realizar transação, quando o botão é clicado', () => {
+    const funcaoRealizarTransação = jest.fn()
+
+    render(<Conta saldo={1000} realizarTransacao={funcaoRealizarTransação} />)
+
+    fireEvent.click(screen.getByText('Realizar operação'));
+
+    expect(funcaoRealizarTransação).toHaveBeenCalled()
   })
 })
