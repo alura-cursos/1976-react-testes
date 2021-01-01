d673b070eb2276f5d9c0de3f9ea98a427aeae33e
PHDeveloper <phbm@hotmail.com>
2021-01-01 13:40:59 -0300

    Clean code

--- src/App.test.js
+++ src/App.test.js
@@ -22,14 +22,5 @@ describe('Componente principal', () => {
                 font-weight: bold;
           `)
         })
-        // it('Verificando a cor do fundo', () => {
-        //     render(<App/>)
-        //     const header = screen.getByTestId('header')
-        //     const bg = header.parentElement;
-        //     expect(bg).toHaveStyle(`
-        //         display: block;
-        //     `)
-        // })
-
     })
 })
