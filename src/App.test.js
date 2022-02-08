43f0888f1a429d74f1cfb27c5d3035ce37fe7e9a
Vanessa Motta <mottavns@gmail.com>
2022-02-07 22:01:29 -0300

    Organizando o arquivo de teste

--- src/App.test.js
+++ src/App.test.js
@@ -3,26 +3,30 @@ import { render, screen } from "@testing-library/react"
 import App from './App';
 
 describe('Componente principal', () => {
-    it('Mostrar o nome do banco', () => {
-        render(<App />);
+	describe('Quando eu abro o app do banco', () => {
+		it('o nome é exibido', () => {
+			render(<App />);
 
-        expect(screen.getByText('ByteBank')).toBeInTheDocument();
-    })
+			expect(screen.getByText('ByteBank')).toBeInTheDocument();
+		})
+		it('o saldo é exibido', () => {
+			render(<App />);
+			
+			expect(screen.getByText('Saldo:')).toBeInTheDocument();
+		})
+		it('O botão de realizar operação é exibido', () => {
+			render(<App />);
+
+			expect(screen.getByText('Realizar operação')).toBeInTheDocument();
+		})
+	})
 })
 
-
 /*
-
 getByText -- BUSCAR PELO TEXTO DESEJADO
 
 toBeInTheDocument -- QUERO SABER SE ELE ESTÁ NO DOCUMENTO.
-   
 
-
-
-
-
-
-
+it pode ser substituido por test
 
 */
